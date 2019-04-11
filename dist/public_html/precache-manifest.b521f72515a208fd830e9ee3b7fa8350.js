a27890b17d00a1d877f673d4324f8f3d113b55f1
Mark Kuznetsov <m.kuznetsov@storverk.com>
2019-04-11 14:08:53 +0300

    rebuild

---
+++ dist/public_html/precache-manifest.b521f72515a208fd830e9ee3b7fa8350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b5df5aa8c58c718fc53e",
+    "url": "/js/chunk-vendors.79c68409.js"
+  },
+  {
+    "revision": "093881a7c2bc0d24597b",
+    "url": "/js/app.76ae29c3.js"
+  },
+  {
+    "revision": "d7c849ad540c6c2a0455",
+    "url": "/js/about.ad763791.js"
+  },
+  {
+    "revision": "eb9ad67d22ab4e77e72e5c28d0a6f039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "093881a7c2bc0d24597b",
+    "url": "/css/app.baf800ae.css"
+  }
+];
